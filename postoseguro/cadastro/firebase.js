507b893bab48a8d39497a2e15cec10032357da7c
apedroknt <127742517+apedroknt@users.noreply.github.com>
2024-04-02 15:31:59 -0300

    Add files via upload

---
+++ postoseguro/cadastro/firebase.js
@@ -0,0 +1,30 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAnF9iQcvhhvvvl8tuG9zmmUwzCMYUQaks",
+    authDomain: "posto-seguro.firebaseapp.com",
+    projectId: "posto-seguro",
+    storageBucket: "posto-seguro.appspot.com",
+    messagingSenderId: "815696307715",
+    appId: "1:815696307715:web:b3db9344cf117e7b8e9036"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  auth.languageCode = 'pt'
+  const provider = new GoogleAuthProvider();
+  const googleLogin = document.getElementById("google-login-btn");
+  googleLogin.addEventListener("click", function(){
+    signInWithPopup(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const user = result.user;
+    console.log(user);
+    window.location.href = "./Site.html"
+  
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+  })
